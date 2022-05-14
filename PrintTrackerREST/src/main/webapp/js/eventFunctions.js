66ceb748922600e7ef6b9fa49175d4c37539cd88
breckiam <breckiam@gmail.com>
2022-05-13 18:21:55 -0600

    Create implemented

--- PrintTrackerREST/src/main/webapp/js/eventFunctions.js
+++ PrintTrackerREST/src/main/webapp/js/eventFunctions.js
@@ -28,6 +28,7 @@ function init() {
 		let stlUrl = document.createPrint.stlUrl.value;
 		if (name != "" && stlUrl != "") {
 			console.log("not null reqirements met");
+			createPrint()
 		} else {
 			let p = document.createElement('p');
 			p.textContent = '* Print Name and STL url are required';
@@ -99,4 +100,49 @@ function getAllPrints() {
 	};
 
 	xhr.send();
+}
+
+function createPrint() {
+
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/prints', true);
+
+	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON request body
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
+				let print = JSON.parse(xhr.responseText);
+				console.log(print);
+			}
+			else {
+				console.log('Print was not created');
+			}
+		}
+	};
+
+	// JavaScript data (object)
+	let userPrint = {
+		name: document.createPrint.printName.value,
+        stlFileUrl: document.createPrint.stlUrl.value,
+        customGcodeUrl: document.createPrint.gcodeUrl.value,
+        printerName: document.createPrint.printerName.value,
+        filamentType: document.createPrint.filamentType.value,
+        filamentBrand: document.createPrint.filamentBrand.value,
+        printTemp: document.createPrint.printTemp.value,
+        printSpeed: document.createPrint.printSpeed.value,
+        adhesionLayer: document.createPrint.adhesionLayer.value,
+        printQuality: document.createPrint.printQuality.value,
+        supports: document.createPrint.supports.value,
+        printImgUrl: document.createPrint.imageUrl.value,
+        creates: document.createPrint.creates.value,
+        lastDateCreated: document.createPrint.createDate.value,
+
+	};
+
+	let userPrintJson = JSON.stringify(userPrint); // Convert JS object to JSON string
+	
+
+	// Pass JSON as request body
+	xhr.send(userPrintJson);
 }
