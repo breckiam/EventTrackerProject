404b1c743bf75e14bebff63bb1e0288d5fe417ad
breckiam <breckiam@gmail.com>
2022-05-22 00:42:33 -0600

    Connected search button to module with search input, method added for telling if number or string was entered into search

--- ngThreeDPrint/src/app/components/navigation/navigation.component.ts
+++ ngThreeDPrint/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+  closeResult: string = '';
+  searchInput: string = '';
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  idOrKeyword(search: string) {
+    console.log(parseInt(search))
+    if (isNaN(parseInt(search))) {
+      console.log('String');
+    } else {
+      console.log('Number');
+    }
   }
 
 }
